a4a529b07518fc6486e982edb927563426f430f8
Tyler <realpsych22@gmail.com>
2020-04-14 23:21:18 -0500

    works!

--- server.js
+++ server.js
@@ -2,13 +2,11 @@
 
 var express = require('express');
 var cors = require('cors');
-var multer = require(multer);
-
-// require and use "multer"...
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
-app.use(multer);
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -16,8 +14,19 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.redirect('/hello?name='+req.file.originalname+"&size="+req.file.size)
+  
+})
+
 app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+  res.json({name: req.query.name, size: req.query.size});
 });
 
 app.listen(process.env.PORT || 3000, function () {
